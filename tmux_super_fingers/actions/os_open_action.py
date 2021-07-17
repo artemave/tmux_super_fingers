5f54f0dc3c22ea61c5bc401fde0bbbee42c41142
artemave <artemave@gmail.com>
2021-07-17 12:28:47 +0200

    Move some code around

--- tmux_super_fingers/actions/os_open_action.py
+++ tmux_super_fingers/actions/os_open_action.py
@@ -3,11 +3,11 @@ from ..utils import shell
 from .action import Action
 from ..targets import OsOpenable
 
-is_macos: bool = 'darwin' in sys.platform.lower()
-
 class OsOpenAction(Action):
     target: OsOpenable
 
     def perform(self) -> None:
+        is_macos = 'darwin' in sys.platform.lower()
         os_open = 'open' if is_macos else 'xdg-open'
+
         shell(f'{os_open} {self.target.file_or_url}')
--- tmux_super_fingers/actions/send_to_vim_in_tmux_window_action.py
+++ tmux_super_fingers/actions/send_to_vim_in_tmux_window_action.py
@@ -10,16 +10,21 @@ class SendToVimInTmuxWindowAction(Action):
         window_names = shell('tmux list-windows -F #{window_name}').split('\n')
         vim_window_names = list(filter(lambda x: x == 'vim' or x == 'nvim', window_names))
 
-        if len(vim_window_names) > 0:
-            os.system('tmux select-window -t %s' %(vim_window_names[0]))
-            vim_pane_id = shell('tmux list-panes -F "#{pane_id}" -t %s' %(vim_window_names[0])).split('\n')[0]
-            os.system('tmux send-keys -t %s Escape' %(vim_pane_id))
+        vim_window_name = vim_window_names[0]
 
-            if self.target.line_number:
-                os.system('tmux send-keys -t %s ":e +%s %s" Enter zz' %(vim_pane_id, self.target.line_number, self.target.file_path))
-            else:
-                os.system('tmux send-keys -t %s ":e %s" Enter zz' %(vim_pane_id, self.target.file_path))
-
+        if vim_window_name:
+            os.system(f'tmux select-window -t {vim_window_name}')
+            vim_pane_id = shell(
+                'tmux list-panes -F "#{pane_id}" -t %s' %(vim_window_name)
+            ).split('\n')[0]
+            os.system(f'tmux send-keys -t {vim_pane_id} Escape')
+            os.system(f'tmux send-keys -t {vim_pane_id} ":e {self._vim_e_args()}" Enter zz')
             return
 
         raise Exception('Could not find "vim" or "nvim" window in the current session')
+
+    def _vim_e_args(self) -> str:
+        if self.target.line_number:
+            return f'+{self.target.line_number} {self.target.file_path}'
+
+        return f'{self.target.file_path}'
